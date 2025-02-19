b4b84fb765652555fd0dc6e7e25b81327fbec27b
bendalejatin <bendalejatin35@gmail.com>
2025-02-19 21:26:28 +0530

    Privacy Policy of DECKIDZ!!!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import PrivacyPolicy from "./Components/PrivacyPolicy";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/privacypolicy" replace />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
